b73d73cb81d0f80a879f0c2f2b04ef4239bd961b
secynic <secynic@gmail.com>
2016-09-24 03:49:37 -0500

    timezone tests

--- nfsinkhole/tests/test_iptables.py
+++ nfsinkhole/tests/test_iptables.py
@@ -1,8 +1,9 @@
 import logging
 from nfsinkhole.exceptions import (IPTablesError, IPTablesExists,
-                                   IPTablesNotExists)
+                                   IPTablesNotExists, SubprocessError)
 from nfsinkhole.iptables import IPTablesSinkhole
 from nfsinkhole.tests import TestCommon
+from nfsinkhole.utils import (popen_wrapper, set_system_timezone)
 
 LOG_FORMAT = ('[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)s] '
               '[%(funcName)s()] %(message)s')
@@ -29,3 +30,18 @@ class TestIPTablesSinkhole(TestCommon):
 
         # Exists
         self.assertRaises(IPTablesExists, myobj.create_drop_rule)
+
+    def test_timezone(self):
+
+        set_system_timezone('UTC')
+
+        # Remove /etc/localtime
+        out, err = popen_wrapper(['rm', '/etc/localtime'],
+                                 raise_err=True, sudo=True)
+
+        # stdout is not expected on success.
+        if out and len(out) > 0:
+            raise SubprocessError(out)
+
+        # localtime is removed, set_system_timezone should fail on copy attempt
+        self.assertRaises(SubprocessError, set_system_timezone, 'UTC')
