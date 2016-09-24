93e244ae8f163e5bbf7e156697065e488e46bb72
secynic <secynic@gmail.com>
2016-09-24 03:09:42 -0500

    nose integration testing

---
+++ nfsinkhole/tests/test_iptables.py
@@ -0,0 +1,31 @@
+import logging
+from nfsinkhole.exceptions import (IPTablesError, IPTablesExists,
+                                   IPTablesNotExists)
+from nfsinkhole.iptables import IPTablesSinkhole
+from nfsinkhole.tests import TestCommon
+
+LOG_FORMAT = ('[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)s] '
+              '[%(funcName)s()] %(message)s')
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+log = logging.getLogger(__name__)
+
+
+class TestIPTablesSinkhole(TestCommon):
+
+    def test_drop(self):
+
+        # Bad interface
+        myobj = IPTablesSinkhole(
+            interface='~!@#$%^&*()',
+        )
+        myobj.create_drop_rule()
+        self.assertRaises(IPTablesExists, myobj.create_drop_rule)
+
+        # Success
+        myobj = IPTablesSinkhole(
+            interface='eth1',
+        )
+        myobj.create_drop_rule()
+
+        # Exists
+        self.assertRaises(IPTablesExists, myobj.create_drop_rule)
