5ae4145dd5b268bd88c2f9543c18026af7991046
secynic <secynic@gmail.com>
2016-10-14 17:16:17 -0500

    removed repo token as test, reset token due to log disclosure

--- nfsinkhole/tests/travis_tests.sh
+++ nfsinkhole/tests/travis_tests.sh
@@ -22,7 +22,7 @@ if [ "${TRAVIS_PYTHON_VERSION}" = "2.7" ]; then
     sudo docker exec nfsinkholevm /bin/sh -c "ls -al /root/nfsinkhole"
     sudo docker exec nfsinkholevm /bin/sh -c "cd /root/nfsinkhole/ && python setup.py install"
     sudo docker exec nfsinkholevm /bin/sh -c "nosetests -v -w /root/nfsinkhole/nfsinkhole --include=docker --with-coverage --cover-package=nfsinkhole"
-    sudo docker exec nfsinkholevm /bin/sh -c "COVERALLS_REPO_TOKEN=${COVERALLS_REPO_TOKEN} coveralls --rcfile=.coveragerc"
+    sudo docker exec nfsinkholevm /bin/sh -c "coveralls --rcfile=/root/nfsinkhole/.coveragerc"
     sudo docker exec --privileged nfsinkholevm /bin/sh -c "python /usr/bin/nfsinkhole-setup.py --interface eth1 --install --pcap --loglevel debug"
     sudo docker exec nfsinkholevm /bin/sh -c "cat /var/log/nfsinkhole-setup.log && rm /var/log/nfsinkhole-setup.log"
     sudo docker exec --privileged nfsinkholevm /bin/sh -c "systemctl start nfsinkhole.service"
